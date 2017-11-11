a89d76c8eda5355ea5ae405f82c0e902ecc48019
didi <1130477946@qq.com>
2017-11-11 13:59:16 +0800

    提交

---
+++ src/dailyrecord/AddLog.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-form :model="formdate">
+    <el-form-item label="标题"
+                  prop="title">
+      <el-input></el-input>
+    </el-form-item>
+    <el-form-item label="内容"
+                  prop="content">
+      <el-input type="textarea"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary">确定</el-button>
+      <el-button type="cancel">取消</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      formdate: {}
+    }
+  }
+}
+</script>
--- src/dailyrecord/index.vue
+++ src/dailyrecord/index.vue
@@ -1,65 +1,69 @@
 <template>
   <div id="main">
+    <el-button @click="addlog = true">新增</el-button>
+      <el-dialog title="新增日志" :visible.sync="addlog">
+        <add-log></add-log>
+      </el-dialog>
     <el-table :data="datab"
               width="100%"
               stripe
               border
               ref="multipleTable">
-      <el-table-column type="selection" width="55">
+      <el-table-column type="selection"
+                       width="55">
       </el-table-column>
       <el-table-column label="日期"
                        width="350"
-                       prop="date">
+                       prop="summary">
       </el-table-column>
       <el-table-column label="人员"
                        width="250"
-                       prop="name"></el-table-column>
+                       prop="user"></el-table-column>
       <el-table-column label="标题"
-                       prop="title"
+                       prop="content"
                        width="840">
-
       </el-table-column>
       <el-table-column label="操作"
-                       width="407">
-
+                       width="405">
         <template slot-scope="scope">
-        <el-button
-          size="mini"
-          @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-        <el-button
-          size="mini"
-          type="danger"
-          @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-      </template>
-
+          <el-button size="mini"
+                     @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-button size="mini"
+                     type="danger"
+                     @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
       </el-table-column>
     </el-table>
   </div>
 </template>
 <script>
+import AddLog from '@/dailyrecord/AddLog'
 export default {
   data () {
     return {
-      datab: [{
-        date: '2017-11-10',
-        name: '王小虎',
-        title: '我还好吗？'
-      },
-      {
-        date: '2017-11-10',
-        name: 'hh',
-        title: 'rwer'
-      },
-      {
-        date: '2017-11-10',
-        name: 'hh',
-        title: 'rwer'
-      }]
+      datab: [],
+      addlog: false
     }
+  },
+  methods: {
+    loadlist () {
+      this.$axios.get('http://qianjia.space:8000/logs')
+        .then(res => {
+          this.datab = res.data
+          console.log('rwer', this.datab)
+        })
+    }
+  },
+  mounted () {
+    this.loadlist()
+  },
+  components: {
+    AddLog
   }
 }
 </script>
 <style>
+
 </style>
 
 
